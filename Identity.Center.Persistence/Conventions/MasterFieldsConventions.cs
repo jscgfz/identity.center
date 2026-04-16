149997f986572b915b7519edd6e2fd5406fe8beb
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:31:53 -0500

    Add: jscg | MasterFieldsConventions

---
+++ Identity.Center.Persistence/Conventions/MasterFieldsConventions.cs
@@ -0,0 +1,27 @@
+﻿using Identity.Center.Domain.Primitives.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Identity.Center.Persistence.Conventions;
+
+internal sealed class MasterFieldsConventions : IModelFinalizingConvention
+{
+  public void ProcessModelFinalizing(IConventionModelBuilder modelBuilder, IConventionContext<IConventionModelBuilder> context)
+  {
+    foreach(IConventionEntityType entityType in modelBuilder.Metadata.GetEntityTypes().Where(IsInherited))
+    {
+      IConventionProperty name = entityType.FindProperty(nameof(IMasterFields.Name)) ?? throw new NullReferenceException();
+      name.SetColumnName(nameof(name));
+      IConventionProperty description = entityType.FindProperty(nameof(IMasterFields.Description)) ?? throw new NullReferenceException();
+      description.SetColumnName(nameof(description));
+      IConventionIndex index = entityType.AddIndex(name) ?? throw new NullReferenceException();
+      index.SetIsUnique(true);
+    }
+  }
+
+  public bool IsInherited(IConventionEntityType entityType)
+    => entityType.ClrType is { IsAbstract: false, IsInterface: false } &&
+      entityType.ClrType.GetInterfaces().Any(i => i == typeof(IMasterFields));
+}
