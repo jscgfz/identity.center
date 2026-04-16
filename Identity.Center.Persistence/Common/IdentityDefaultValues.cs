e3cfcaf688361585fcbf7d4046863127e32ffd1a
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:29:59 -0500

    Add: jscg | IdentityDefaultValues

---
+++ Identity.Center.Persistence/Common/IdentityDefaultValues.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace Identity.Center.Persistence.Common;
+
+internal static class IdentityDefaultValues
+{
+  public const string UtcNow = "getutcdate()";
+  public const string Guid = "newsequentialid()";
+  public static JsonElement EmptyJson => JsonSerializer.Deserialize<JsonElement>("{}");
+}
