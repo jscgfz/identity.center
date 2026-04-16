716dc86462484c51c6d05b68eb209bd4a526617c
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:36:02 -0500

    Add: jscg | IGlobalAtomicValues

---
+++ Identity.Center.Application/Abstractions/Data/IGlobalAtomicValues.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Center.Application.Abstractions.Data;
+
+public interface IGlobalAtomicValues<TUser>
+  where TUser : struct, IEquatable<TUser>
+{
+  TUser UserId { get; }
+  DateTime UtcDatime { get; }
+}
