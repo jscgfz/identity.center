4c81e43b18f3a9c63bb380f19f8c7e877f49c179
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:25:27 -0500

    Add: jscg | IdentityCommons encoding and hash key provider

---
+++ Identity.Center.Domain/Common/IdentityCommons.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Identity.Center.Domain.Common;
+
+public static class IdentityCommons
+{
+  public static Encoding Encoding => Encoding.UTF8;
+  public static byte[] NewHashKey => RandomNumberGenerator.GetBytes(32);
+}
