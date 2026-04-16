ae9e25dea4dc3b111f05f3c397fb76494db348eb
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:24:35 -0500

    Add: jscg | HealtCheckTypes enum

---
+++ Identity.Center.Domain/Enums/HealtCheckTypes.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Center.Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum HealtCheckTypes
+{
+  [JsonStringEnumMemberName("mssql")] SQLServer,
+  [JsonStringEnumMemberName("postgre")] Postgre,
+  [JsonStringEnumMemberName("mongodb")] Mongo,
+  [JsonStringEnumMemberName("redis")] Redis,
+  [JsonStringEnumMemberName("kafka")] Kafka,
+  [JsonStringEnumMemberName("rabbitmq")] RabbitMQ
+}
