2e633498d0c57c7b467acf6aac2112f6ba5c7132
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:25:51 -0500

    Add: jscg | Identity named Schemas

---
+++ Identity.Center.Domain/Constants/IdentitySchemas.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity.Center.Domain.Constants;
+
+public static class IdentitySchemas
+{
+  public const string Identity = "id";
+  public const string Security = "sec";
+  public const string Builds = "build";
+  public const string Authentication = "auth";
+  public const string Authorization = Authentication;
+}
