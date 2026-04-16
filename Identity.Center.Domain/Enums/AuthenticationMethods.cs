2dbefc8f9847c904681dc203c65585ee81ba4efd
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:22:14 -0500

    Add: jscg | AuthenticationMethods enum

---
+++ Identity.Center.Domain/Enums/AuthenticationMethods.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Center.Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum AuthenticationMethods
+{
+  [JsonStringEnumMemberName("user-password")] Single,
+  [JsonStringEnumMemberName("qd-endpoint")] Quamtum,
+  [JsonStringEnumMemberName("ldap")] LDAP,
+}
