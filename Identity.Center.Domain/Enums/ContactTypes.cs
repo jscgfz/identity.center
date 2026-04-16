52612a810b733acdd2ca3d2a41015112c44f3916
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:23:05 -0500

    Add: jscg | ContactTypes enum

---
+++ Identity.Center.Domain/Enums/ContactTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Center.Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum ContactTypes
+{
+  [JsonStringEnumMemberName("coorp-mail")] CorporativeMail,
+  [JsonStringEnumMemberName("external-mail")] ExternalMail,
+  [JsonStringEnumMemberName("cellphone")] Cellphone
+}
