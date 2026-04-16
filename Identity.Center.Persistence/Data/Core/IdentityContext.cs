3a4056d0d9b74a7dfcaf94172b946b3ef16669c0
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-16 16:33:57 -0500

    Add: jscg | IdentityContext

---
+++ Identity.Center.Persistence/Data/Core/IdentityContext.cs
@@ -0,0 +1,22 @@
+﻿using Identity.Center.Persistence.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Center.Persistence.Data.Core;
+
+// dotnet ef migrations add {} -c IdentityContext -s ./Identity.Center.Api -p ./Identity.Center.Persistence -o Data/Core/Migrations -v
+internal sealed class IdentityContext(DbContextOptions options) : DbContext(options)
+{
+  protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+  {
+    base.ConfigureConventions(
+      configurationBuilder.WithIdentityConventions()
+    );
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(
+      modelBuilder.WithSchemas()
+    );
+  }
+}
