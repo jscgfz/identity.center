f868ae2b62ce25f78df04de1a644f2f927fc8267
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-17 16:25:13 -0500

    Add: jscg | IdentityClaimTypes

---
+++ Identity.Center.Domain/Constants/IdentityClaimTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace Identity.Center.Domain.Constants;
+
+public static class IdentityClaimTypes
+{
+  public const string App = "http://schemas.identity.com/identity/claims/app";
+  public const string Root = "http://schemas.identity.com/identity/claims/root";
+}
