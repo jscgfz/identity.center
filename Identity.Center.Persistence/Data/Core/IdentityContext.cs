f3d527192b7ae12c8a4d702e72df6063b2223fba
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-17 16:31:36 -0500

    Add: jscg | Initial Migrations

--- Identity.Center.Persistence/Data/Core/IdentityContext.cs
+++ Identity.Center.Persistence/Data/Core/IdentityContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Identity.Center.Persistence.Data.Core;
 
 // dotnet ef migrations add {} -c IdentityContext -s ./Identity.Center.Api -p ./Identity.Center.Persistence -o Data/Core/Migrations -v
-internal sealed class IdentityContext(DbContextOptions options) : DbContext(options)
+public sealed class IdentityContext(DbContextOptions options) : DbContext(options)
 {
   protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
   {
