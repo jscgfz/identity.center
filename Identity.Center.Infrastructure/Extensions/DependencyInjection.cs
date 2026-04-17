15858ec2f51ef663acc28a075100422713e3a517
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-17 16:30:26 -0500

    Add: jscg | DI Authentication extension metohds

---
+++ Identity.Center.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Identity.Center.Infrastructure.Configuration.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Center.Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+  public static WebApplicationBuilder WithAuthentication(this WebApplicationBuilder builder)
+  {
+    builder
+      .Services
+      .ConfigureOptions<JwtOptionsConfigurer>();
+
+    builder
+      .Services
+      .AddAuthentication(options =>
+      {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      })
+      .AddJwtBearer();
+
+    return builder;
+  }
+}
