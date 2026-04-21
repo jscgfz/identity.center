cbcf400162d125058d677533fe6e62b7fb9b21d0
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-21 18:32:05 -0500

    Add: jscg | ClaimsManager contract

---
+++ Identity.Center.Application/Abstractions/Managers/IClaimsManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Identity.Center.Application.Abstractions.Managers;
+
+public interface IClaimsManager
+{
+  Task<IEnumerable<Claim>> ByRole(Guid roleId, CancellationToken cancellationToken = default);
+  Task<IEnumerable<Claim>> ByApkiKey(Guid apiKey, CancellationToken cancellationToken = default);
+}
