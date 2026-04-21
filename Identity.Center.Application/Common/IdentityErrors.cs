756f236e4ddb1ec39915b8d60c41d26bdacfa8ec
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-21 18:32:23 -0500

    Add: jscg | IdentityErrors

---
+++ Identity.Center.Application/Common/IdentityErrors.cs
@@ -0,0 +1,9 @@
+﻿using Identity.Center.Application.Abstractions.Result;
+using Identity.Center.Application.Result;
+
+namespace Identity.Center.Application.Common;
+
+public static class IdentityErrors
+{
+  public static IError NotFound => new BaseError("Object.NotFound", "No se encontró información");
+}
