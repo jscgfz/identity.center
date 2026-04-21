fe61a4e08f6c4caf9e4632ad18beb4cfdee6e425
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-21 18:39:49 -0500

    Add: jscg | ApiKeySchemeHandler

---
+++ Identity.Center.Infrastructure/Configuration/Authentication/ApiKeySchemeHandler.cs
@@ -0,0 +1,84 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Identity.Center.Domain.Common;
+using Identity.Center.Domain.Common.Models.Cryptography;
+using Identity.Center.Domain.Constants;
+using Identity.Center.Domain.Entities.Core.Authentication;
+using Identity.Center.Persistence.Data.Core;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+
+namespace Identity.Center.Infrastructure.Configuration.Authentication;
+
+public sealed class ApiKeySchemeHandler(
+  IOptionsMonitor<ApiKeySchemeOptions> option,
+  ILoggerFactory logger,
+  UrlEncoder encoder
+) : AuthenticationHandler<ApiKeySchemeOptions>(option, logger, encoder)
+{
+  protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+  {
+    IdentityContext context = Request.HttpContext.RequestServices.GetRequiredService<IdentityContext>();
+    CancellationToken cancellationToken = Request.HttpContext.RequestAborted;
+
+    if (
+      !Request.Headers.TryGetValue(Options.HeaderName, out StringValues header) ||
+      header.FirstOrDefault() is not string apikeyValue ||
+      !apikeyValue.Contains(':')
+    )
+      return AuthenticateResult.Fail(
+        new NullReferenceException(
+          $"{Options.HeaderName} Header not Found"
+        )
+      );
+
+    string subject = apikeyValue[..apikeyValue.LastIndexOf(':')];
+    string apiKey = apikeyValue[(apikeyValue.LastIndexOf(':') + 1)..];
+
+    if (
+      !Guid.TryParse(subject, out Guid subjectId) ||
+      !await context.Set<ApiKey>().AnyAsync(row => row.Id == subjectId, cancellationToken)
+    )
+      return AuthenticateResult.Fail(
+        new InvalidDataException(
+          $"Invalid subject ${subject}"
+        )
+      );
+
+    ApiKey dbApikey = await context.Set<ApiKey>()
+      .AsNoTracking()
+      .FirstAsync(row => row.Id == subjectId, cancellationToken);
+
+    HashValidationResponse hashValidation = await IdentityCommons.ValidateHash(
+      new(
+        apiKey,
+        dbApikey.Hash,
+        dbApikey.Salt
+      ),
+      cancellationToken
+    );
+
+    if (!hashValidation.Success)
+      return AuthenticateResult.Fail(
+        new UnauthorizedAccessException(
+          "Invalid ApiKey"
+        )
+      );
+
+    IEnumerable<Claim> initialClaims = [
+      new Claim(ClaimTypes.NameIdentifier, subjectId.ToString("N")),
+      new Claim(IdentityClaimTypes.App, dbApikey.AppId.ToString("N")),
+      new Claim(IdentityClaimTypes.Root, dbApikey.Root.ToString())
+    ];
+
+    ClaimsIdentity identity = new(initialClaims, nameof(ApiKeySchemeHandler));
+    ClaimsPrincipal principal = new(identity);
+    AuthenticationTicket ticket = new(principal, Scheme.Name);
+
+    return AuthenticateResult.Success(ticket);
+  }
+}
---
+++ Identity.Center.Infrastructure/Configuration/Authentication/ApiKeySchemeOptions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Identity.Center.Infrastructure.Configuration.Authentication;
+
+public sealed class ApiKeySchemeOptions : AuthenticationSchemeOptions
+{
+  public const string DefaultScheme = "IdentityApiKeyScheme";
+  public string HeaderName { get; set; } = "x-api-key";
+}
