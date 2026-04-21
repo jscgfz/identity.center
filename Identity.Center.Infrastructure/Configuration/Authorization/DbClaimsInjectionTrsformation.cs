5b9eb6d428553376615500c4fe7135e648b073f4
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-21 18:40:02 -0500

    Add: scg | DbClaimsInjectionTrsformation

---
+++ Identity.Center.Infrastructure/Configuration/Authorization/DbClaimsInjectionTrsformation.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Identity.Center.Application.Abstractions.Managers;
+using Identity.Center.Domain.Entities.Core.Authentication;
+using Identity.Center.Domain.Entities.Core.Identity;
+using Identity.Center.Persistence.Data.Core;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Identity.Center.Infrastructure.Configuration.Authorization;
+
+internal sealed class DbClaimsInjectionTrsformation(IServiceProvider provider) : IClaimsTransformation
+{
+  private readonly IClaimsManager _claimsManager = provider.GetRequiredService<IClaimsManager>();
+  private readonly IdentityContext _context = provider.GetRequiredService<IdentityContext>();
+  private readonly IDatabase _redis = provider.GetRequiredService<IDatabase>();
+
+  public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+  {
+    IEnumerable<Claim> claims = [];
+    if (principal.FindFirstValue(ClaimTypes.NameIdentifier) is string name && Guid.TryParse(name, out Guid identifier))
+      if (await _context.Set<User>().AnyAsync(row => row.Id == identifier))
+      {
+        if (
+          principal.FindFirstValue(ClaimTypes.Role) is not string role ||
+          !Guid.TryParse(role, out Guid roleGuid)
+        )
+          return principal;
+
+        claims = await _claimsManager.ByRole(identifier);
+      }
+      else if (await _context.Set<ApiKey>().AnyAsync(row => row.Id == identifier))
+        claims = await _claimsManager.ByApkiKey(identifier);
+
+    ClaimsIdentity identity = new(claims);
+    principal.AddIdentity(identity);
+    return principal;
+  }
+}
