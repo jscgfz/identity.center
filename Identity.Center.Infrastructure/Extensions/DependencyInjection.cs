33f929f9f22218db6b4015d84ea28e5d721c0808
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-21 18:40:52 -0500

    Fix: jscg | Infra DI

--- Identity.Center.Infrastructure/Extensions/DependencyInjection.cs
+++ Identity.Center.Infrastructure/Extensions/DependencyInjection.cs
@@ -1,13 +1,20 @@
-﻿using Identity.Center.Infrastructure.Configuration.Authentication;
+﻿using Identity.Center.Application.Abstractions.Managers;
+using Identity.Center.Infrastructure.Configuration.Authentication;
+using Identity.Center.Infrastructure.Configuration.Authorization;
+using Identity.Center.Infrastructure.Managers;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using StackExchange.Redis;
 
 namespace Identity.Center.Infrastructure.Extensions;
 
 public static class DependencyInjection
 {
-  public static WebApplicationBuilder WithAuthentication(this WebApplicationBuilder builder)
+  public static WebApplicationBuilder WithAuth(this WebApplicationBuilder builder)
   {
     builder
       .Services
@@ -21,7 +28,31 @@ public static class DependencyInjection
         options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
       })
-      .AddJwtBearer();
+      .AddJwtBearer()
+      .AddScheme<ApiKeySchemeOptions, ApiKeySchemeHandler>(
+        ApiKeySchemeOptions.DefaultScheme,
+        default
+      );
+
+    builder
+      .Services
+      .TryAddEnumerable([
+        ServiceDescriptor.Transient<IClaimsTransformation, DbClaimsInjectionTrsformation>(),
+        ServiceDescriptor.Transient<IClaimsManager, ClaimsManager>()
+      ]);
+
+    return builder;
+  }
+
+  public static WebApplicationBuilder WithCache(this WebApplicationBuilder builder)
+  {
+
+    builder
+      .Services
+      .AddSingleton<IConnectionMultiplexer>(sp => ConnectionMultiplexer.Connect(sp.GetRequiredService<IConfiguration>().GetConnectionString(nameof(IRedis))!));
+    builder
+      .Services
+      .AddSingleton(sp => sp.GetRequiredService<IConnectionMultiplexer>().GetDatabase());
 
     return builder;
   }
