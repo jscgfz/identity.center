cb0920407da57a74b634558bd02ee0a6471b7b7f
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-21 18:33:37 -0500

    Fix: jscg | Application DI

---
+++ Identity.Center.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Identity.Center.Application.Result.Behaviors;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Center.Application.Extensions;
+
+public static class DependencyInjection
+{
+  public static WebApplicationBuilder WithResultExtensions(this WebApplicationBuilder builder)
+  {
+    Assembly assembly = Assembly.GetExecutingAssembly();
+    builder
+      .Services
+      .AddMediatR(options =>
+      {
+        options.RegisterServicesFromAssembly(assembly);
+        options.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ResultBreakerBehavior<,>));
+      });
+    builder
+      .Services
+      .AddValidatorsFromAssembly(assembly);
+    return builder;
+  }
+}
