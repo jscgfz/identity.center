4bd693c1a628843cc74c9f2dbff6427863a446e9
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-22 17:04:33 -0500

    Add: jscg | AppsV1Module

---
+++ Identity.Center.Api/Endpoints/V1/AppsV1Module.cs
@@ -0,0 +1,30 @@
+﻿using Identity.Center.Api.Configuration;
+using Identity.Center.Application.Features.Apps.Queries.GetApp;
+using Identity.Center.Application.Features.Apps.Queries.GetApps;
+using Identity.Center.Application.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Center.Api.Endpoints.V1;
+
+public sealed class AppsV1Module : IIdentityModule
+{
+  public void Registry(IEndpointRouteBuilder builder)
+  {
+    RouteGroupBuilder group = builder
+      .MapGroup("/apps")
+      .WithTags("Aplicaciones");
+
+    group.MapGet(
+      string.Empty,
+      async ([AsParameters] GetAppsQuery query, ISender sender) =>
+        await sender.Send(query).AsHttpResult()
+    );
+
+    group.MapGet(
+      "/{appId}",
+      async ([FromRoute] Guid appId, ISender sender) =>
+        await sender.Send(new GetAppQuery(appId)).AsHttpResult()
+    );
+  }
+}
