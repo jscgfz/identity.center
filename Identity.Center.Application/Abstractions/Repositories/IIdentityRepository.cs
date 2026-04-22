50aee4128a7aee214bab1f7dad2d16779bb99e06
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-22 16:58:08 -0500

    Add: jscg | IIdentityRepository contract

---
+++ Identity.Center.Application/Abstractions/Repositories/IIdentityRepository.cs
@@ -0,0 +1,8 @@
+﻿using Identity.Center.Application.Abstractions.Managers;
+
+namespace Identity.Center.Application.Abstractions.Repositories;
+
+public interface IIdentityRepository<TEntity> : IBaseDbRepository<TEntity>
+  where TEntity : class
+{
+}
