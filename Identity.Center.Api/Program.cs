c05195a2f610088429421e45eb797ba317c78fbe
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-22 12:50:44 -0500

    Fix: jscg | DI

--- Identity.Center.Api/Program.cs
+++ Identity.Center.Api/Program.cs
@@ -1,12 +1,18 @@
+using Asp.Versioning.ApiExplorer;
+using Identity.Center.Api.Extensions;
 using Identity.Center.Application.Extensions;
+using Identity.Center.Infrastructure.Configuration.Authentication;
 using Identity.Center.Infrastructure.Extensions;
 using Identity.Center.Persistence.Extensions;
+using Microsoft.AspNetCore.Authorization;
 
 WebApplicationBuilder builder = WebApplication
   .CreateBuilder(args)
   .WithIdentityPersistence()
   .WithAuth()
   .WithCache()
+  .WithVersioning()
+  .WithOpenApiDocumentation("v1")
   .WithResultExtensions();
 
 // Add services to the container.
@@ -17,6 +23,16 @@ WebApplication app = builder.Build();
 
 //app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapOpenApi("/openapi/{documentName}/openapidoc.json");
+app.UseSwaggerUI(options =>
+{
+  IEnumerable<ApiVersionDescription> versions = app.DescribeApiVersions();
+  foreach (ApiVersionDescription version in versions)
+    options.SwaggerEndpoint($"/openapi/{version.GroupName}/openapidoc.json", version.GroupName);
+});
+
 string[] summaries =
 [
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -24,16 +40,17 @@ string[] summaries =
 
 app.MapGet("/weatherforecast", () =>
 {
-  IEnumerable<WeatherForecast> forecast = Enumerable.Range(1, 5).Select(index =>
+  IEnumerable<WeatherForecast> forecast = [.. Enumerable.Range(1, 5).Select(index =>
       new WeatherForecast
       (
           DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
           Random.Shared.Next(-20, 55),
           summaries[Random.Shared.Next(summaries.Length)]
-      ))
-      .ToArray();
+      ))];
   return forecast;
-});
+})
+  .RequireAuthorization()
+  .Produces(StatusCodes.Status401Unauthorized);
 
 app.Run();
 
