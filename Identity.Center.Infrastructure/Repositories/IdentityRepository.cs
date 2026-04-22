485e391a3df719659c2a94978fbebf50df28b711
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-22 17:01:46 -0500

    Add: jscg | IIdentityRepository implementation

---
+++ Identity.Center.Infrastructure/Repositories/IdentityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Center.Application.Abstractions.Repositories;
+using Identity.Center.Persistence.Data.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Center.Infrastructure.Repositories;
+
+internal sealed class IdentityRepository<TEntity>(IServiceProvider provider) : IIdentityRepository<TEntity>
+  where TEntity : class
+{
+  private readonly IdentityContext _context = provider.GetRequiredService<IdentityContext>();
+
+  public IQueryable<TEntity> Data => _context.Set<TEntity>();
+}
