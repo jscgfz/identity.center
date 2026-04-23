29da6353dfca1e7cef6d772ea060fc15a3b36942
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-23 12:14:49 -0500

    Add: jscg | Pagintaion contracts

---
+++ Identity.Center.Application/Abstractions/Common/IPaginatedResult.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Center.Application.Result;
+
+namespace Identity.Center.Application.Abstractions.Common;
+
+public interface IPaginatedResult<TData>
+  where TData : class
+{
+  int Count { get; }
+  int PageCount { get; }
+  int PageIndex { get; }
+  int PageSize { get; }
+  IEnumerable<TData> Data { get; }
+}
---
+++ Identity.Center.Application/Abstractions/Common/IPaginationParams.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Center.Application.Abstractions.Common;
+
+public interface IPaginationParams
+{
+  int? PageIndex { get; }
+  int? PageSize { get; }
+  bool? FullSet { get; }
+}
