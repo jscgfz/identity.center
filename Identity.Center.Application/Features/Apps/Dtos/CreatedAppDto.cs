d2e432be24d26df414b67e191161879eb13a479d
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-23 17:35:12 -0500

    Add: jscg | CreatedAppDto

---
+++ Identity.Center.Application/Features/Apps/Dtos/CreatedAppDto.cs
@@ -0,0 +1,7 @@
+﻿using Identity.Center.Application.Abstractions.Reponses;
+
+namespace Identity.Center.Application.Features.Apps.Dtos;
+
+public sealed record CreatedAppDto(
+  Guid Id
+) : ICreatedResponse;
