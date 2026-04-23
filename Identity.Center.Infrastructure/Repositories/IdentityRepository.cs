b0fa629d1374e45352ea9bc74176b4a92eaa67fb
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-23 12:28:43 -0500

    Add: jscg | IdentityRepository base Repo implementatijon

--- Identity.Center.Infrastructure/Repositories/IdentityRepository.cs
+++ Identity.Center.Infrastructure/Repositories/IdentityRepository.cs
@@ -1,13 +1,11 @@
-﻿using Identity.Center.Application.Abstractions.Repositories;
+﻿using Identity.Center.Application.Abstractions.Managers;
+using Identity.Center.Application.Abstractions.Repositories;
 using Identity.Center.Persistence.Data.Core;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Identity.Center.Infrastructure.Repositories;
 
-internal sealed class IdentityRepository<TEntity>(IServiceProvider provider) : IIdentityRepository<TEntity>
+internal sealed class IdentityRepository<TEntity>(IServiceProvider provider) : BaseDbRepository<IdentityContext, TEntity>(provider)
   where TEntity : class
-{
-  private readonly IdentityContext _context = provider.GetRequiredService<IdentityContext>();
-
-  public IQueryable<TEntity> Data => _context.Set<TEntity>();
-}
+{ }
