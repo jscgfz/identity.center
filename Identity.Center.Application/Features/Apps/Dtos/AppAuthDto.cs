0bb5fb9936b1c8a514920917551be9f379202e6a
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-23 12:16:24 -0500

    Add: jscg | AppAuthDto

---
+++ Identity.Center.Application/Features/Apps/Dtos/AppAuthDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity.Center.Application.Features.Apps.Dtos;
+
+public sealed record AppAuthDto(
+  Guid Id,
+  byte[] SignatureKey,
+  bool TwoFactorEnabled,
+  TimeSpan ExpirationTime,
+  TimeSpan RefreshTime,
+  DateTimeOffset CreatedAtUtc
+);
