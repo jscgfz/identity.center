dc6950d11f5aa7fcd881a4580bda57a99a4ecb18
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-23 12:14:20 -0500

    Add: jscg | IBaseDbRepository entries contracts methods

--- Identity.Center.Application/Abstractions/Managers/IBaseDbRepository.cs
+++ Identity.Center.Application/Abstractions/Managers/IBaseDbRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace Identity.Center.Application.Abstractions.Managers;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Identity.Center.Application.Abstractions.Managers;
 
 public interface IBaseDbRepository<TEntity>
   where TEntity : class
 {
   IQueryable<TEntity> Data { get; }
+  ValueTask<EntityEntry<TEntity>> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+  Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+  EntityEntry<TEntity> Remove(TEntity entity);
+  void RemoveRange(IEnumerable<TEntity> entities);
+  EntityEntry<TEntity> Update(TEntity entity);
+  void UpdateRange(IEnumerable<TEntity> entities);
 }
