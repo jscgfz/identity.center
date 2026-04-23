233066fecc5a01dd614dfd32abb6d80f71788341
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-23 12:15:36 -0500

    Add: jscg | Initial Unit of works

---
+++ Identity.Center.Application/Abstractions/Managers/IBaseUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Identity.Center.Application.Abstractions.Managers;
+
+public interface IBaseUnitOfWork
+{
+  Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+  IDbContextTransaction? CurrentTransaction { get; }
+}
---
+++ Identity.Center.Application/Abstractions/Repositories/IIdentityUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿using Identity.Center.Application.Abstractions.Managers;
+
+namespace Identity.Center.Application.Abstractions.Repositories;
+
+public interface IIdentityUnitOfWork : IBaseUnitOfWork
+{
+}
