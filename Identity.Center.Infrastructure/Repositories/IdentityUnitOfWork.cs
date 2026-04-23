c22eccd33b54cde06905e8729689191c725e0b69
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-04-23 12:36:11 -0500

    Add: jscg | IIdentityUnitOfWork implementation

---
+++ Identity.Center.Infrastructure/Repositories/IdentityUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Identity.Center.Application.Abstractions.Repositories;
+using Identity.Center.Persistence.Data.Core;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Center.Infrastructure.Repositories;
+
+internal sealed class IdentityUnitOfWork(IServiceProvider provider) : IIdentityUnitOfWork
+{
+  private readonly IdentityContext _context = provider.GetRequiredService<IdentityContext>();
+  public IDbContextTransaction? CurrentTransaction => _context.Database.CurrentTransaction;
+
+  public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    => _context.SaveChangesAsync(cancellationToken);
+}
