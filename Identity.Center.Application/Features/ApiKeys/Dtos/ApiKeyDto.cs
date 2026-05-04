2b129000679bdcce3c7abdd490e88f9a31c01f19
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-04 09:05:42 -0500

    Add: jscg | Api keys dtos

---
+++ Identity.Center.Application/Features/ApiKeys/Dtos/ApiKeyDto.cs
@@ -0,0 +1,12 @@
+﻿using Identity.Center.Application.Features.Apps.Dtos;
+
+namespace Identity.Center.Application.Features.ApiKeys.Dtos;
+
+public sealed record ApiKeyDto(
+  Guid SubjectId,
+  string Name,
+  string? Description,
+  AppDto? App,
+  DateTimeOffset CreatedAtUtc,
+  IEnumerable<string> Claims
+);
---
+++ Identity.Center.Application/Features/ApiKeys/Dtos/CreatedApkiKeyDto.cs
@@ -0,0 +1,8 @@
+﻿using Identity.Center.Application.Abstractions.Reponses;
+
+namespace Identity.Center.Application.Features.ApiKeys.Dtos;
+
+public sealed record CreatedApkiKeyDto(
+  Guid SubjectId,
+  string ApiKey
+) : ICreatedResponse;
