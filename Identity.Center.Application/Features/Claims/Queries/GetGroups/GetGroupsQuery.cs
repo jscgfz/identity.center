055df1d3f3056da5104877cef742088f63c72f2e
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-04 09:08:54 -0500

    Add: jscg | GetGroupsQuery

---
+++ Identity.Center.Application/Features/Claims/Queries/GetGroups/GetGroupsQuery.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Center.Application.Abstractions.Common;
+using Identity.Center.Application.Abstractions.Result;
+using Identity.Center.Application.Features.Claims.Dtos;
+
+namespace Identity.Center.Application.Features.Claims.Queries.GetGroups;
+
+public sealed record GetGroupsQuery(
+  string? Name,
+  string? Description,
+  int? PageIndex,
+  int? PageSize,
+  bool? FullSet
+) : IPaginationParams, IQuery<IPaginatedResult<MasterClaimPart>>;
---
+++ Identity.Center.Application/Features/Claims/Queries/GetGroups/GetGroupsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Identity.Center.Application.Abstractions.Common;
+using Identity.Center.Application.Abstractions.Repositories;
+using Identity.Center.Application.Abstractions.Result;
+using Identity.Center.Application.Common;
+using Identity.Center.Application.Features.Claims.Dtos;
+using Identity.Center.Application.Result;
+using Identity.Center.Domain.Entities.Core.Security;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Center.Application.Features.Claims.Queries.GetGroups;
+
+internal sealed class GetGroupsQueryHandler(IServiceProvider provider) : IQueryHandler<GetGroupsQuery, IPaginatedResult<MasterClaimPart>>
+{
+  private readonly IIdentityRepository<Group> _repo = provider.GetRequiredService<IIdentityRepository<Group>>();
+
+  public async Task<Result<IPaginatedResult<MasterClaimPart>>> Handle(GetGroupsQuery request, CancellationToken cancellationToken)
+  {
+    IPaginatedResult<MasterClaimPart> result = await PaginatedResult.ComputeAsync(
+      _repo.Data
+        .Where(row => string.IsNullOrWhiteSpace(request.Name) || row.Name.Contains(request.Name))
+        .Where(row => string.IsNullOrWhiteSpace(request.Description) || (!string.IsNullOrWhiteSpace(row.Description) && row.Description.Contains(request.Description)))
+        .OrderByDescending(row => row.CreatedAtUtc)
+        .Select(row => new MasterClaimPart(
+          row.Id,
+          row.Name,
+          row.Description,
+          row.CreatedAtUtc
+        )),
+      request,
+      cancellationToken
+    );
+
+    return result.AsResult();
+  }
+}
