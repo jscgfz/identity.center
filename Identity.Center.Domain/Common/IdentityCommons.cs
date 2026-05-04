111fd4452559aec14aff1d289549d8738f366ebe
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-04 08:46:24 -0500

    Add: jscg | IdentityCommons hash creation

--- Identity.Center.Domain/Common/IdentityCommons.cs
+++ Identity.Center.Domain/Common/IdentityCommons.cs
@@ -8,6 +8,19 @@ public static class IdentityCommons
 {
   public static Encoding Encoding => Encoding.UTF8;
   public static byte[] NewHashKey => RandomNumberGenerator.GetBytes(32);
+  public static async Task<HashCreationResponse> NewHash(string? value = null, CancellationToken cancellationToken = default)
+  {
+    byte[] salt = NewHashKey;
+    value ??= Encoding.GetString(NewHashKey);
+    using HMACSHA256 hmac = new(salt);
+    using MemoryStream memo = new(Encoding.GetBytes(value));
+    byte[] hash = await hmac.ComputeHashAsync(memo, cancellationToken).ConfigureAwait(false);
+    return new(
+      salt,
+      hash,
+      value
+    );
+  }
   public static async Task<HashValidationResponse> ValidateHash(HashValidationRequest request, CancellationToken cancellationToken = default)
   {
     using HMACSHA256 hmac = new(request.Salt);
