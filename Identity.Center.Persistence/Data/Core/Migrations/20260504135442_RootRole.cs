d671e0199c03cdbade54e50b6c9d46b92bc5ef1b
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-04 08:55:49 -0500

    Add: jscg | root roles migrations

---
+++ Identity.Center.Persistence/Data/Core/Migrations/20260504135442_RootRole.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Center.Persistence.Data.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RootRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "root",
+                schema: "id",
+                table: "roles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "root",
+                schema: "id",
+                table: "roles");
+        }
+    }
+}
