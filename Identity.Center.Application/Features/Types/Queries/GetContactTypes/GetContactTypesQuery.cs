414d5a7f164b7bedf598985a5d095886ddc24738
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-04 09:04:55 -0500

    Add: jscg | GetContactTypesQuery

---
+++ Identity.Center.Application/Features/Types/Queries/GetContactTypes/GetContactTypesQuery.cs
@@ -0,0 +1,7 @@
+﻿using Identity.Center.Application.Abstractions.Result;
+using Identity.Center.Application.Common.Response;
+using Identity.Center.Domain.Enums;
+
+namespace Identity.Center.Application.Features.Types.Queries.GetContactTypes;
+
+public sealed record GetContactTypesQuery() : IQuery<IEnumerable<MasterOption<ContactTypes>>>;
---
+++ Identity.Center.Application/Features/Types/Queries/GetContactTypes/GetContactTypesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Identity.Center.Application.Abstractions.Repositories;
+using Identity.Center.Application.Abstractions.Result;
+using Identity.Center.Application.Common.Response;
+using Identity.Center.Application.Result;
+using Identity.Center.Domain.Entities.Core.Security;
+using Identity.Center.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Center.Application.Features.Types.Queries.GetContactTypes;
+
+internal sealed class GetContactTypesQueryHandler(IServiceProvider provider) : IQueryHandler<GetContactTypesQuery, IEnumerable<MasterOption<ContactTypes>>>
+{
+  private readonly IIdentityRepository<ContactType> _repo = provider.GetRequiredService<IIdentityRepository<ContactType>>();
+
+  public async Task<Result<IEnumerable<MasterOption<ContactTypes>>>> Handle(GetContactTypesQuery request, CancellationToken cancellationToken)
+  {
+    IEnumerable<MasterOption<ContactTypes>> options = await _repo.Data
+      .Select(row => new MasterOption<ContactTypes>(
+        row.ContactTypeKey,
+        row.Name
+      ))
+      .ToListAsync(cancellationToken);
+
+    return options.Success();
+  }
+}
