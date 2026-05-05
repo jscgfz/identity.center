c0f63bfc9860be51d3a41b33bc4f9de80c82de88
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-05 08:58:32 -0500

    Add: jscg | IdentityAuthorizationMiddlewareResultHandler

---
+++ Identity.Center.Infrastructure/Configuration/Authorization/IdentityAuthorizationHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Center.Infrastructure.Configuration.Authorization;
+
+internal sealed class IdentityAuthorizationHandler : IAuthorizationMiddlewareResultHandler
+{
+  private readonly AuthorizationMiddlewareResultHandler _defaultHandler = new();
+
+  public async Task HandleAsync(
+    RequestDelegate next,
+    HttpContext context,
+    AuthorizationPolicy policy,
+    PolicyAuthorizationResult authorizeResult
+  )
+  {
+    if (authorizeResult.Forbidden)
+    {
+      string failureReason = authorizeResult.AuthorizationFailure?.FailureReasons.FirstOrDefault()?.Message
+                          ?? "No cumples con los requisitos de seguridad para este recurso.";
+
+      ProblemDetails problemDetails = new()
+      {
+        Status = StatusCodes.Status403Forbidden,
+        Title = "Acceso Denegado (Forbidden)",
+        Detail = failureReason,
+        Instance = context.Request.Path,
+        Type = "https://tools.ietf.org/html/rfc7231#section-6.5.3"
+      };
+
+      if (authorizeResult.AuthorizationFailure != null)
+      {
+        IEnumerable<IAuthorizationRequirement> requirementsNames = authorizeResult.AuthorizationFailure.FailedRequirements;
+        problemDetails.Extensions["failedRequirements"] = requirementsNames.Select(req => JsonSerializer.SerializeToElement(req, req.GetType(), JsonSerializerOptions.Web));
+      }
+
+      context.Response.StatusCode = StatusCodes.Status403Forbidden;
+      context.Response.ContentType = "application/problem+json";
+
+      await context.Response.WriteAsJsonAsync(problemDetails);
+      return;
+    }
+
+    await _defaultHandler.HandleAsync(next, context, policy, authorizeResult);
+  }
+}
