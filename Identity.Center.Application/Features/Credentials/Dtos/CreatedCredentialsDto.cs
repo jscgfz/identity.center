1e12759c7c836bda3fe1af4291c1fa8548280121
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-07 08:45:04 -0500

    Add: jscg | credential dtos

---
+++ Identity.Center.Application/Features/Credentials/Dtos/CreatedCredentialsDto.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Center.Application.Abstractions.Reponses;
+
+namespace Identity.Center.Application.Features.Credentials.Dtos;
+
+public sealed class CreatedCredentialsDto : List<CreatedCredentialDto>, ICreatedResponse
+{ }
+
+public sealed record CreatedCredentialDto(
+  Guid? Id,
+  Guid? AppId,
+  int CredentialTypeId,
+  string Value
+);
---
+++ Identity.Center.Application/Features/Credentials/Dtos/CredentialRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Identity.Center.Application.Features.Credentials.Dtos;
+public sealed record CredentialRequestDto(
+  int CredentialTypeId,
+  Guid? AppId,
+  string Value
+);
