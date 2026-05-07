7048363386dd2d754e316864b8b91c2126001182
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-07 08:41:59 -0500

    Add: jscg | RelatedClaimDto

---
+++ Identity.Center.Application/Features/Claims/Dtos/RelatedClaimDto.cs
@@ -0,0 +1,5 @@
+﻿using Identity.Center.Application.Abstractions.Reponses;
+
+namespace Identity.Center.Application.Features.Claims.Dtos;
+public sealed class RelatedClaimDto : List<string>, ICreatedResponse
+{ }
