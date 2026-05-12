97bbdcfdbb58ba5f6ec3a39c36be5d79cbceb176
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-12 17:35:59 -0500

    Add: jscg | Roles endpoints (partial)

---
+++ Identity.Center.Api/Endpoints/V1/RolesV1Module.cs
@@ -0,0 +1,23 @@
+﻿using Identity.Center.Api.Configuration;
+using Identity.Center.Application.Features.Roles.Commands.AddRole;
+using Identity.Center.Application.Features.Roles.Dtos;
+using Identity.Center.Application.Result;
+using MediatR;
+
+namespace Identity.Center.Api.Endpoints.V1;
+
+public sealed class RolesV1Module : IIdentityModule
+{
+  public void Registry(IEndpointRouteBuilder builder)
+  {
+    RouteGroupBuilder group = builder
+      .MapGroup("/roles")
+      .WithTags("roles");
+
+    group
+      .MapPost(string.Empty, async (AddRoleCommand cmd, ISender sender) =>
+        await sender.Send(cmd).AsHttpResult()
+      )
+      .Produces<CreatedRoleDto>();
+  }
+}
