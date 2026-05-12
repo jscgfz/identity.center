df65f872d9f3699959a912b08fc348e681aca4b5
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-12 17:24:42 -0500

    Add: jscg | BasicUserInfoDto Credentials Add: jscg | BasicUserInfoDto Roles

--- Identity.Center.Application/Features/Users/Dtos/BasicUserInfoDto.cs
+++ Identity.Center.Application/Features/Users/Dtos/BasicUserInfoDto.cs
@@ -1,4 +1,7 @@
-﻿namespace Identity.Center.Application.Features.Users.Dtos;
+﻿using Identity.Center.Application.Features.Credentials.Dtos;
+using Identity.Center.Application.Features.Roles.Dtos;
+
+namespace Identity.Center.Application.Features.Users.Dtos;
 
 public record BasicUserInfoDto(
   Guid Id,
@@ -9,5 +12,17 @@ public record BasicUserInfoDto(
   string FirstLastName,
   string? SecondLastName,
   DateTimeOffset CreatedAtUtc,
-  IEnumerable<ContactInfoDto> ContactInfo
-);
+  IEnumerable<ContactInfoDto> ContactInfo,
+  IEnumerable<CreatedCredentialDto>? DomainCredentials,
+  IEnumerable<CreatedCredentialDto>? SingleCredentials,
+  IEnumerable<RoleDto>? Roles
+)
+{
+  private readonly IEnumerable<CreatedCredentialDto>? DomainCredentials = DomainCredentials;
+  private readonly IEnumerable<CreatedCredentialDto>? SingleCredentials = SingleCredentials;
+
+  public IEnumerable<CreatedCredentialDto>? Credentials => (DomainCredentials is null || !DomainCredentials.Any()) && (SingleCredentials is null || !SingleCredentials.Any()) ? null : [
+    .. (DomainCredentials ?? Enumerable.Empty<CreatedCredentialDto>()),
+    .. (SingleCredentials ?? Enumerable.Empty<CreatedCredentialDto>())
+  ];
+}
