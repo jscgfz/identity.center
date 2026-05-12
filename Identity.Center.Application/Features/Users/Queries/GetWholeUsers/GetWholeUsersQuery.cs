74f3a2adeb798daab87381dc8a9402d32cfcdf0c
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-12 17:26:34 -0500

    Add: jscg | GetWholeUsersQuery

---
+++ Identity.Center.Application/Features/Users/Queries/GetWholeUsers/GetWholeUsersQuery.cs
@@ -0,0 +1,18 @@
+﻿using Identity.Center.Application.Abstractions.Common;
+using Identity.Center.Application.Abstractions.Result;
+using Identity.Center.Application.Features.Users.Dtos;
+
+namespace Identity.Center.Application.Features.Users.Queries.GetWholeUsers;
+
+public sealed record GetWholeUsersQuery(
+  int? PageIndex,
+  int? PageSize,
+  bool? FullSet,
+  string? DocumentType,
+  string? DocumentNumber,
+  string? FirstName,
+  string? SecondName,
+  string? FirstLastName,
+  string? SecondLastName,
+  string? ContactValue
+) : IPaginationParams, IQuery<IPaginatedResult<BasicUserInfoDto>>;
---
+++ Identity.Center.Application/Features/Users/Queries/GetWholeUsers/GetWholeUsersQueryHandler.cs
@@ -0,0 +1,79 @@
+﻿using Identity.Center.Application.Abstractions.Common;
+using Identity.Center.Application.Abstractions.Repositories;
+using Identity.Center.Application.Abstractions.Result;
+using Identity.Center.Application.Common;
+using Identity.Center.Application.Features.Credentials.Dtos;
+using Identity.Center.Application.Features.Roles.Dtos;
+using Identity.Center.Application.Features.Users.Dtos;
+using Identity.Center.Application.Result;
+using Identity.Center.Domain.Entities.Core.Builds;
+using Identity.Center.Domain.Entities.Core.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Center.Application.Features.Users.Queries.GetWholeUsers;
+
+internal sealed class GetWholeUsersQueryHandler(IServiceProvider provider) : IQueryHandler<GetWholeUsersQuery, IPaginatedResult<BasicUserInfoDto>>
+{
+  private readonly IIdentityRepository<User> _repo = provider.GetRequiredService<IIdentityRepository<User>>();
+
+  public async Task<Result<IPaginatedResult<BasicUserInfoDto>>> Handle(GetWholeUsersQuery request, CancellationToken cancellationToken)
+  {
+    IPaginatedResult<BasicUserInfoDto> result = await PaginatedResult.ComputeAsync(
+      _repo.Data
+        .Where(row => string.IsNullOrWhiteSpace(request.DocumentType) || row.DocumentType.Contains(request.DocumentType))
+        .Where(row => string.IsNullOrWhiteSpace(request.DocumentNumber) || row.DocumentNumber.Contains(request.DocumentNumber))
+        .Where(row => string.IsNullOrWhiteSpace(request.FirstName) || row.FirstName.Contains(request.FirstName))
+        .Where(row => string.IsNullOrWhiteSpace(request.FirstLastName) || row.FirstLastName.Contains(request.FirstLastName))
+        .Where(row => string.IsNullOrWhiteSpace(request.SecondName) || (!string.IsNullOrWhiteSpace(row.SecondName) && row.SecondName.Contains(request.SecondName)))
+        .Where(row => string.IsNullOrWhiteSpace(request.SecondLastName) || (!string.IsNullOrWhiteSpace(row.SecondLastName) && row.SecondLastName.Contains(request.SecondLastName)))
+        .Where(row => string.IsNullOrWhiteSpace(request.ContactValue) || row.ContactInfo.Any(info => info.Value.Contains(request.ContactValue)))
+        .OrderByDescending(row => row.CreatedAtUtc)
+        .Select(row => new BasicUserInfoDto(
+          row.Id,
+          row.DocumentType,
+          row.DocumentNumber,
+          row.FirstName,
+          row.SecondName,
+          row.FirstLastName,
+          row.SecondLastName,
+          row.CreatedAtUtc,
+          row.ContactInfo.Select(info => new ContactInfoDto(
+            info.Id,
+            null,
+            info.ContactType.ContactTypeKey,
+            info.Value,
+            info.Confirmed,
+            info.CreatedAtUtc
+          )),
+          row.DomainCredentials.Select(dc => new CreatedCredentialDto(
+              dc.Id,
+              null,
+              dc.CredentialTypeId,
+              dc.Username,
+              dc.CreatedAtUtc
+          )),
+          row.SingleCredentials.Select(sc => new CreatedCredentialDto(
+            null,
+            null,
+            default,
+            sc.Username,
+            sc.CreatedAtUtc
+          )),
+          row.Roles
+            .Select(r => new RoleDto(
+              r.RoleId,
+              r.Role.AppId,
+              r.Role.Name,
+              r.Role.Description,
+              r.Role.DomainName,
+              r.Role.Root,
+              r.Role.CreatedAtUtc
+            ))
+        )),
+      request,
+      cancellationToken
+    );
+
+    return result.AsResult();
+  }
+}
