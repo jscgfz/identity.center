2d450403ddab0d37b35c468a987e5f8411302974
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 14:07:28 -0500

    Add: jscg | NotificationBuilder contract

---
+++ Identity.Center.Application/Abstractions/Notifications/INotificationBuilder.cs
@@ -0,0 +1,10 @@
+﻿using Identity.Center.Application.Result;
+using MediatR;
+
+namespace Identity.Center.Application.Abstractions.Notifications;
+
+public interface INotificationBuilder
+{
+  Task<Result<Unit>> DispachAsync(IServiceProvider provider, CancellationToken cancellationToken);
+  Task<Result<Unit>> EnQueueAsync(IServiceProvider provider, CancellationToken cancellationToken);
+}
