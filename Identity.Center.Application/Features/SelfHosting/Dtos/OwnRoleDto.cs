ead1aa539d3be443906d933a710d455577390ea8
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 14:18:01 -0500

    Add: jscg | SelfHosting dtos

---
+++ Identity.Center.Application/Features/SelfHosting/Dtos/OwnRoleDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Identity.Center.Application.Features.SelfHosting.Dtos;
+
+public sealed record OwnRoleDto(
+  Guid Id,
+  string Name,
+  string? Description,
+  string? DomainName,
+  bool ActiveDiretoryMandatory,
+  bool Root,
+  DateTimeOffset CreatedAtUtc,
+  IEnumerable<string> Claims
+);
---
+++ Identity.Center.Application/Features/SelfHosting/Dtos/OwnUserDto.cs
@@ -0,0 +1,24 @@
+﻿using Identity.Center.Domain.Enums;
+
+namespace Identity.Center.Application.Features.SelfHosting.Dtos;
+
+public sealed record OwnUserDto(
+  Guid Id,
+  string DocumentType,
+  string DocumentNumber,
+  string FirstName,
+  string? SecondName,
+  string FirstLastName,
+  string? SecondLastName,
+  DateTimeOffset CratedAtUtc,
+  IEnumerable<OwnContactInfoDto> ContactInfo,
+  IEnumerable<Guid> Roles
+);
+
+
+public sealed record OwnContactInfoDto(
+  ContactTypes Type,
+  string Value,
+  bool Confirmed,
+  DateTimeOffset CratedAtUtc
+);
