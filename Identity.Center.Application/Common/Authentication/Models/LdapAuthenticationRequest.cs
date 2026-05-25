8b882c3bef7d7cac85d67056647d875aaff75d41
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 13:01:04 -0500

    Add: jscg | LdapAuthentication models

---
+++ Identity.Center.Application/Common/Authentication/Models/LdapAuthenticationRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Center.Application.Common.Authentication.Models;
+
+public record LdapAuthenticationRequest(
+  [property: JsonPropertyName("usuario")] string UserName,
+  [property: JsonPropertyName("clave")] string Password,
+  [property: JsonPropertyName("dominio")] string? DomainName,
+  [property: JsonPropertyName("firma")] string? Key
+);
---
+++ Identity.Center.Application/Common/Authentication/Models/LdapAuthenticationResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Center.Application.Common.Authentication.Models;
+
+public sealed record LdapAuthenticationResponse(
+  [property: JsonPropertyName("mensaje")]  LdapAuthenticationMessage Message,
+  IEnumerable<LdapAuthenticationRole> Roles
+);
+
+public sealed record LdapAuthenticationMessage(
+  [property: JsonPropertyName("codigoMensaje")] int Code,
+  [property: JsonPropertyName("descMensaje")] string Description
+);
+
+public sealed record LdapAuthenticationRole(
+  [property: JsonPropertyName("descripcion")] string Description
+);
