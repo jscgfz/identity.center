7c98ec83bb70950f551f975b1f7ab2eaec4ea8d2
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 14:09:15 -0500

    Add: jscg | BaseFileRender

---
+++ Identity.Center.Application/Common/BaseFileRender.cs
@@ -0,0 +1,17 @@
+﻿using Identity.Center.Application.Abstractions.Reponses;
+
+namespace Identity.Center.Application.Common;
+
+public sealed record BaseFileRender(
+  byte[] Content,
+  string Name,
+  string MimeType
+) : IFileResponse
+{
+  public FileContentResponse Render()
+    => new(
+      Content,
+      Name,
+      MimeType
+    );
+}
