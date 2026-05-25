124fc136169f08b0908764c58fd50b105035be8b
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 13:01:18 -0500

    Add: jscg | IQdControlClient

---
+++ Identity.Center.Application/Abstractions/Clients/IQdControlClient.cs
@@ -0,0 +1,10 @@
+﻿using Identity.Center.Application.Common.Authentication.Models;
+using Refit;
+
+namespace Identity.Center.Application.Abstractions.Clients;
+
+public interface IQdControlClient
+{
+  [Post("/ValidaUsuario")]
+  Task<HttpResponseMessage> Validate(LdapAuthenticationRequest request, CancellationToken cancellationToken);
+}
