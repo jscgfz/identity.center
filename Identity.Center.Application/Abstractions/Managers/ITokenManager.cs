f914189d5fe42b92fbabd4640d31240a3d485db2
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 14:08:02 -0500

    Add: jscg | TokenManager contract

---
+++ Identity.Center.Application/Abstractions/Managers/ITokenManager.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Center.Application.Features.Authentication.Dtos;
+using Identity.Center.Application.Result;
+using Identity.Center.Domain.Enums;
+using MediatR;
+
+namespace Identity.Center.Application.Abstractions.Managers;
+
+public interface ITokenManager
+{
+  Task<Result<AuthenticationReponseDto>> FromUser(Guid userId, Guid appId, IEnumerable<string>? domainRoles = null, MfaStates? mfaOverride = null, CancellationToken cancellationToken = default);
+  Task<Result<string>> RefreshToken(CancellationToken cancellationToken);
+  Task<Result<Unit>> ValidateSession();
+}
