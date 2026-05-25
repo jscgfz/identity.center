a18184b6ba0dc20786ebf3d6202114365fec1e06
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 12:59:03 -0500

    Add: jscg | Masivian clients

---
+++ Identity.Center.Application/Abstractions/Clients/IMasivianMailClient.cs
@@ -0,0 +1,10 @@
+﻿using Identity.Center.Application.Common.Notifications.Masivian.Mail.Request;
+using Refit;
+
+namespace Identity.Center.Application.Abstractions.Clients;
+
+public interface IMasivianMailClient
+{
+  [Post("v1/delivery")]
+  Task<HttpResponseMessage> DeliveryV1([Body] MasivianEmailRequest request, CancellationToken cancellationToken);
+}
---
+++ Identity.Center.Application/Abstractions/Clients/IMasivianSmsClient.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Center.Application.Common.Notifications.Masivian.Sms.Request;
+using Refit;
+
+namespace Identity.Center.Application.Abstractions.Clients;
+
+public interface IMasivianSmsClient
+{
+  [Post("/send-message")]
+  Task<HttpResponseMessage> SendMessage([Body] MasivianSmsRequest request, CancellationToken cancellationToken);
+
+  [Post("/send-message-batch")]
+  Task<HttpResponseMessage> SendMessage([Body] IEnumerable<MasivianSmsRequest> request, CancellationToken cancellationToken);
+}
