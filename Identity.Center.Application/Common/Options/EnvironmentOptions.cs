7c966007669fe55f23fec5500182475ba81c82f5
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 12:44:23 -0500

    Add: jscg | EnvironmentOptions

---
+++ Identity.Center.Application/Common/Options/EnvironmentOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Identity.Center.Application.Common.Options;
+
+public sealed class EnvironmentOptions
+{
+  public required IEnumerable<string> DevEnvironments { get; set; }
+  public bool IsDevEnvironment(string environmentName)
+    => DevEnvironments.Contains(environmentName);
+
+  public bool IsDevEnvironment(ref IServiceProvider provider)
+    => IsDevEnvironment(provider.GetRequiredService<IWebHostEnvironment>());
+
+  public bool IsDevEnvironment<THost>(THost host) where THost : IHostEnvironment
+    => IsDevEnvironment(host.EnvironmentName);
+}
