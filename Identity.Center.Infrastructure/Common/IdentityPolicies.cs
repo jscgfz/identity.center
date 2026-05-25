5ba4aafa2b29c0341e4fc845220dc1fc7551510b
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 14:20:18 -0500

    Add: jscg | IdentityPolicies

---
+++ Identity.Center.Infrastructure/Common/IdentityPolicies.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Identity.Center.Domain.Common;
+
+namespace Identity.Center.Infrastructure.Common;
+
+public static class IdentityPolicies
+{
+  public const string MfaPending = "jwt-mfa-pending";
+  public const string Jwt = "jwt";
+  public const string ApiKey = "api-key";
+  public const string Root = "cprt-root";
+
+  public static string FromClaim([RegularExpression(@"^[a-z]*\:[a-z]*$")] string claim)
+    => IdentityCommons.FromClaim(claim);
+}
