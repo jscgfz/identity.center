c2442983bf04ea60628e66cf10c142fd0d6fccf5
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 12:52:04 -0500

    Add: jscg | MasivianSmsRequest

---
+++ Identity.Center.Application/Common/Notifications/Masivian/Sms/Request/MasivianSmsRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Identity.Center.Application.Common.Notifications.Masivian.Sms.Request;
+
+public sealed record MasivianSmsRequest(
+  string To,
+  string? Text,
+  int? IdTemplate,
+  string? CustomData,
+  bool IsPremium = false,
+  bool IsFlash = true,
+  bool? IsRandomRoute = null,
+  MasivianSmsShortUrlRequest? ShortUrlConfig = null,
+  Dictionary<string, string>? ReplacementFields = null
+)
+{
+  public bool IsLongMessage => !string.IsNullOrEmpty(Text) &&
+    (
+      (!IsPremium && Text.Length > 160) ||
+      (IsPremium && Text.Length > 70)
+    );
+}
---
+++ Identity.Center.Application/Common/Notifications/Masivian/Sms/Request/MasivianSmsShortUrlRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Identity.Center.Application.Common.Notifications.Masivian.Sms.Request;
+
+public sealed record MasivianSmsShortUrlRequest(
+  string Url,
+  string? DomainShortUrl = null
+);
