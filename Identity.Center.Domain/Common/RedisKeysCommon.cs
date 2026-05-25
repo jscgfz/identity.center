455dfa7994402a85686826d7c3cdcd8e539f951a
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 12:36:18 -0500

    Add: jscg | RedisKeysCommon SessionHashKey

--- Identity.Center.Domain/Common/RedisKeysCommon.cs
+++ Identity.Center.Domain/Common/RedisKeysCommon.cs
@@ -4,5 +4,6 @@ public static class RedisKeysCommon
 {
   public static string ApiKeyHashKey(Guid apiKey) => $"apikeys:{apiKey:N}";
   public static string RoleHashKey(Guid roleId) => $"roles:{roleId}";
+  public static string SessionHashKey(Guid userId) => $"sessions:{userId}";
   public static string ClaimsStore => "id:claims";
 }
