40f64a059a01534e6c611c3cea3499320b0491a2
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 14:26:08 -0500

    Add: jscg | JwtOptionsConfigurer Events handling

--- Identity.Center.Infrastructure/Configuration/Authentication/JwtOptionsConfigurer.cs
+++ Identity.Center.Infrastructure/Configuration/Authentication/JwtOptionsConfigurer.cs
@@ -1,44 +1,84 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Identity.Center.Application.Abstractions.Managers;
+using Identity.Center.Application.Result;
 using Identity.Center.Domain.Constants;
 using Identity.Center.Domain.Entities.Core.Builds;
 using Identity.Center.Domain.Entities.Core.Security;
 using Identity.Center.Persistence.Data.Core;
+using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Identity.Center.Infrastructure.Configuration.Authentication;
 
 internal class JwtOptionsConfigurer(IServiceProvider provider) : IConfigureNamedOptions<JwtBearerOptions>
 {
-  private readonly IDbContextFactory<IdentityContext> _factory = provider.GetRequiredService<IDbContextFactory<IdentityContext>>();
+  private readonly IdentityContext _context = provider.GetRequiredService<IDbContextFactory<IdentityContext>>().CreateDbContext();
 
   public void Configure(string? name, JwtBearerOptions options)
   {
-    using IdentityContext context = _factory.CreateDbContext();
     options.TokenValidationParameters = new()
     {
       ValidateLifetime = true,
       ValidateIssuer = true,
       ClockSkew = TimeSpan.Zero,
       LogValidationExceptions = true,
-      RequireAudience = true,
+      RequireAudience = false,
       RequireExpirationTime = true,
       RequireSignedTokens = true,
       ValidAlgorithms = [SecurityAlgorithms.HmacSha256Signature],
-      IssuerSigningKeyResolver = (token, securityToken, kid, validationParameters) => IssuerSigningKeyResolver(token, securityToken, kid, validationParameters, context),
-      ValidIssuers = context.Set<App>().Select(row => row.Prefix).AsEnumerable()
+      IssuerSigningKeyResolver = (token, securityToken, kid, validationParameters) => IssuerSigningKeyResolver(token, securityToken, kid, validationParameters, _context),
+      ValidIssuers = _context.Set<App>().Select(row => row.Prefix).AsEnumerable(),
+      AudienceValidator = (audiences, securityToken, validationParameters) => true
     };
+    options.Events = new()
+    {
+      OnMessageReceived = context =>
+      {
+        if(context.Request.Query.TryGetValue("access_token", out StringValues accesToken))
+          context.Token = accesToken.Single();
+        if (context.Request.Cookies.TryGetValue("accesToken", out string? cookieToken))
+          context.Token = cookieToken;
+        return Task.CompletedTask;
+      },
+      OnAuthenticationFailed = async context =>
+      {
+        if(context.Exception is SecurityTokenExpiredException)
+        {
+          Result<string> refresh = await context.HttpContext.RequestServices.GetRequiredService<ITokenManager>()
+            .RefreshToken(context.HttpContext.RequestAborted);
 
+          if (refresh.Success)
+          {
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+            ClaimsPrincipal principal = jwtSecurityTokenHandler.ValidateToken(refresh.Value, context.Options.TokenValidationParameters, out _);
+            context.Principal = principal;
+            context.Success();
+          }
+          else
+            context.Fail(refresh.Errors.First().Seralize().Value?.ToString() ?? "Unknown reasons");
+        }
+      },
+      OnTokenValidated = async context =>
+      {
+        Result<Unit> sessionValidation = await context.HttpContext.RequestServices.GetRequiredService<ITokenManager>()
+          .ValidateSession();
+
+        if (sessionValidation.Failed)
+          context.Fail(string.Join(", ", sessionValidation.Errors.Select(e => e.Seralize().Value)));
+      }
+    };
   }
 
   public void Configure(JwtBearerOptions options)
     => Configure(JwtBearerDefaults.AuthenticationScheme, options);
 
-  private IEnumerable<SecurityKey> IssuerSigningKeyResolver(string token, SecurityToken securityToken, string kid, TokenValidationParameters validationParameters, IdentityContext context)
+  private static IEnumerable<SecurityKey> IssuerSigningKeyResolver(string token, SecurityToken securityToken, string kid, TokenValidationParameters validationParameters, IdentityContext context)
   {
     JwtSecurityTokenHandler handler = new();
     JwtSecurityToken jwtSecurityToken = handler.ReadJwtToken(token);
