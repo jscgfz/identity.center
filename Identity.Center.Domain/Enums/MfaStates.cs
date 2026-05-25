21302cf4452771b0efa2dd12e39c4b3769b699d7
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 12:38:19 -0500

    Add: jscg | MfaStates

---
+++ Identity.Center.Domain/Enums/MfaStates.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Center.Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum MfaStates
+{
+  [JsonStringEnumMemberName("pending")] Pending,
+  [JsonStringEnumMemberName("passed")] Passed,
+  [JsonStringEnumMemberName("not-required")] NotRequired
+}
