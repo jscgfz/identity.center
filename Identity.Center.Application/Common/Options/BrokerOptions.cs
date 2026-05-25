83c838284b2e91c2e55547ac0b3807994627193f
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 12:44:00 -0500

    Fix: jscg | BrokerOptions serialization methods

--- Identity.Center.Application/Common/Options/BrokerOptions.cs
+++ Identity.Center.Application/Common/Options/BrokerOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Text.Json.Serialization;
+using Identity.Center.Domain.Common;
 using Identity.Center.Domain.Enums;
 
 namespace Identity.Center.Application.Common.Options;
@@ -8,16 +9,8 @@ public sealed class BrokerOptions
 {
   private const string TopicPrefix = "comms";
   private const string QueuePrefix = "queue";
-  private static string Name(ContactTypes v)
-  {
-    Type type = typeof(ContactTypes);
-    MemberInfo? memberInfo = type.GetMember(v.ToString()!).FirstOrDefault();
-    return memberInfo is null
-      ? throw new InvalidOperationException()
-      : memberInfo.GetCustomAttribute<JsonStringEnumMemberNameAttribute>()?.Name ?? throw new InvalidOperationException();
-  }
-  public static string Topic(ContactTypes type) => $"{TopicPrefix}.{Name(type)}";
-  public static string Queue(ContactTypes type) => $"{QueuePrefix}.{Name(type)}";
+  public static string Topic(ContactTypes type) => $"{TopicPrefix}.{IdentityCommons.Serialize(type)}";
+  public static string Queue(ContactTypes type) => $"{QueuePrefix}.{IdentityCommons.Serialize(type)}";
   public required string Host { get; set; }
   public required string Username { get; set; }
   public required string Password { get; set; }
