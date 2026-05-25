8a9a67323e8cde2f6ec99818a6233a243f2821dd
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 12:37:57 -0500

    Add: jscg | Mfa and SessionId IdentityClaimTypes

--- Identity.Center.Domain/Constants/IdentityClaimTypes.cs
+++ Identity.Center.Domain/Constants/IdentityClaimTypes.cs
@@ -4,4 +4,6 @@ public static class IdentityClaimTypes
 {
   public const string App = "http://schemas.identity.com/identity/claims/app";
   public const string Caim = "http://schemas.identity.com/identity/claims/claim";
+  public const string Mfa = "http://schemas.identity.com/identity/claims/mfa";
+  public const string SessionId = "http://schemas.identity.com/identity/claims/sid";
 }
