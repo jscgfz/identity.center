2025f8cc9e849c92c14aceac3f07e553d8748ee0
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 12:35:45 -0500

    Add: jscg | Serialization common methods

--- Identity.Center.Domain/Common/IdentityCommons.cs
+++ Identity.Center.Domain/Common/IdentityCommons.cs
@@ -1,5 +1,9 @@
-﻿using System.Security.Cryptography;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using Identity.Center.Domain.Common.Models.Cryptography;
 using Identity.Center.Domain.Entities.Core.Authentication;
@@ -21,7 +25,7 @@ public static class IdentityCommons
   }
   public static Encoding Encoding => Encoding.UTF8;
   public static byte[] NewHashKey => RandomNumberGenerator.GetBytes(32);
-  public static Task<HashCreationResponse> NewHash(string? value = null, CancellationToken cancellationToken = default)
+  public static Task<HashCreationResponse> NewHash(string? value = null)
   {
     byte[] salt = NewHashKey;
     value ??= Encoding.GetString(NewHashKey);
@@ -41,7 +45,7 @@ public static class IdentityCommons
       )
     );
   }
-  public static Task<HashValidationResponse> ValidateHash(HashValidationRequest request, CancellationToken cancellationToken = default)
+  public static Task<HashValidationResponse> ValidateHash(HashValidationRequest request)
   {
     byte[] hashResult = KeyDerivation.Pbkdf2(
       password: request.Value,
@@ -58,5 +62,42 @@ public static class IdentityCommons
         request.Hash
       )
     );
+  }
+
+  public static string Serialize<TEnum>(TEnum @enum)
+    where TEnum : struct
+  {
+    Type type = typeof(TEnum);
+    MemberInfo? memberInfo = type.GetMember(@enum.ToString()!).FirstOrDefault();
+    return memberInfo is null
+      ? throw new InvalidOperationException()
+      : memberInfo.GetCustomAttribute<JsonStringEnumMemberNameAttribute>()?.Name ?? throw new InvalidOperationException();
+  }
+
+  public static TEnum Deserialize<TEnum>(string value)
+    where TEnum : struct
+  {
+    Type type = typeof(TEnum);
+    MemberInfo? memberInfo = type.GetMembers()
+      .FirstOrDefault(mi => mi.GetCustomAttribute<JsonStringEnumMemberNameAttribute>() is JsonStringEnumMemberNameAttribute attr && attr.Name.Equals(value));
+    return memberInfo is null
+      ? throw new InvalidOperationException()
+      : Enum.Parse<TEnum>(memberInfo.Name);
+  }
+
+  private const string ClaimPart = "cprt-";
+  public static string FromClaim([RegularExpression(@"^[a-z]*\:[a-z]*$")] string claim)
+    => $"{ClaimPart}{claim}";
+  public static bool ValidatePolicyFromClaim(string policyName, [NotNullWhen(true)] out string? claim)
+  {
+#pragma warning disable SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
+    if (Regex.IsMatch(policyName, @$"^{ClaimPart}[a-z]*\:[a-z]*$") || policyName == $"{ClaimPart}root")
+    {
+      claim = policyName.Replace(ClaimPart, string.Empty);
+      return true;
+    }
+#pragma warning restore SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
+    claim = null;
+    return false;
   }
 }
