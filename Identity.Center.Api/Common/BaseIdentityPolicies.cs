ac0d7721160d331de6d6d1d51af0da96754f618a
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-05-25 14:49:55 -0500

    Rm: jscg | IdentityPolicyBuilder

--- Identity.Center.Api/Common/BaseIdentityPolicies.cs
+++
@@ -1,9 +0,0 @@
-﻿using Identity.Center.Api.Configuration.Authorization;
-
-namespace Identity.Center.Api.Common;
-
-public static class BaseIdentityPolicies
-{
-  public static IdentityPolicyBuilder Jwt => IdentityPolicyBuilder.Empty.AllowJtw();
-  public static IdentityPolicyBuilder ApiKey => IdentityPolicyBuilder.Empty.AllowApiKeys();
-}
