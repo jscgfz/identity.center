7abd8e0cdd906038d4a9c680ddcdb763084cd262
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-06-18 09:44:26 -0500

    Add: jscg | Router module entities configuration

---
+++ Identity.Center.Persistence/Configuration/Core/Authorization/RouteClaimConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Identity.Center.Domain.Constants;
+using Identity.Center.Domain.Entities.Core.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.Center.Persistence.Configuration.Core.Authorization;
+
+internal sealed class RouteClaimConfiguration : IEntityTypeConfiguration<RouteClaim>
+{
+  public void Configure(EntityTypeBuilder<RouteClaim> builder)
+  {
+    builder
+      .ToTable("routes_claims", IdentitySchemas.Authorization);
+
+    builder
+      .HasKey(row => new { row.RouteId, row.ClaimId });
+
+    builder
+      .Property(row => row.RouteId)
+      .HasColumnName("route_id");
+
+    builder
+      .Property(row => row.ClaimId)
+      .HasColumnName("claim_id");
+
+    builder
+      .HasOne(row => row.Route)
+      .WithMany(row => row.Claims)
+      .HasForeignKey(row => row.RouteId)
+      .OnDelete(DeleteBehavior.NoAction);
+
+    builder
+      .HasOne(row => row.Claim)
+      .WithMany(row => row.Routes)
+      .HasForeignKey(row => row.ClaimId)
+      .OnDelete(DeleteBehavior.NoAction);
+  }
+}
---
+++ Identity.Center.Persistence/Configuration/Core/Builds/AppRouteConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using Identity.Center.Domain.Constants;
+using Identity.Center.Domain.Entities.Core.Builds;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.Center.Persistence.Configuration.Core.Builds;
+
+internal sealed class AppRouteConfiguration : IEntityTypeConfiguration<AppRoute>
+{
+  public void Configure(EntityTypeBuilder<AppRoute> builder)
+  {
+    builder
+      .ToTable("routes", IdentitySchemas.Builds);
+
+    builder
+      .HasIndex(row => new
+      {
+        row.AppId,
+        row.Key,
+        row.Path
+      })
+      .IsUnique();
+
+    builder
+      .Property(row => row.AppId)
+      .HasColumnName("app_id");
+
+    builder
+      .Property(row => row.Key)
+      .HasColumnName("key");
+
+    builder
+      .Property(row => row.Path)
+      .HasColumnName("path")
+      .HasDefaultValue("/");
+
+    builder
+      .Property(row => row.ExcludeNav)
+      .HasColumnName("exclude_navigation")
+      .HasDefaultValue(true);
+
+    builder
+      .Property(row => row.Index)
+      .HasColumnName("index")
+      .HasDefaultValue(0);
+
+    builder
+      .Property(row => row.Icon)
+      .HasColumnName("icon");
+
+    builder
+      .Property(row => row.ParentRouteId)
+      .HasColumnName("parent_route_id");
+
+    builder
+      .HasOne(row => row.App)
+      .WithMany(row => row.Routes)
+      .HasForeignKey(row => row.AppId)
+      .OnDelete(DeleteBehavior.NoAction);
+
+    builder
+      .HasOne(row => row.ParentRoute)
+      .WithMany(row => row.ChildRoutes)
+      .HasForeignKey(row => row.ParentRouteId)
+      .OnDelete(DeleteBehavior.NoAction);
+  }
+}
