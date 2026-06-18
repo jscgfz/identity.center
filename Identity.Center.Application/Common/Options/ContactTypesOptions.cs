68e7f477bb9b04a3b8f8218fc1e6158933bd1759
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-06-18 09:51:49 -0500

    Add: jscg | ContactTypesOptions

---
+++ Identity.Center.Application/Common/Options/ContactTypesOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Identity.Center.Application.Common.Options;
+
+public sealed class ContactTypesOptions
+{
+  public required IEnumerable<string> CellPhoneExpressions { get; set; }
+  public required IEnumerable<string> EmailExpressions { get; set; }
+}
