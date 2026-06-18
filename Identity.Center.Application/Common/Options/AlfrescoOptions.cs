afb15fd234c9c5db38ffd9e1a8e4ba6b5363083a
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-06-18 09:51:38 -0500

    Add: jscg | AlfrescoOptions

---
+++ Identity.Center.Application/Common/Options/AlfrescoOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity.Center.Application.Common.Options;
+
+public sealed class AlfrescoOptions
+{
+  public required string BaseUrl { get; set; }
+  public required string Username { get; set; }
+  public required string Password { get; set; }
+  public required Dictionary<string, string> NodeCollection { get; set; }
+  public required IEnumerable<string> ValidMimeTypes { get; set; }
+}
