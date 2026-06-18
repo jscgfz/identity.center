1bab61f76b8f428d8400a55f05b4fcebd4320953
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-06-18 09:43:13 -0500

    Add: jscg | ChangeControlStates

---
+++ Identity.Center.Domain/Enums/ChangeControlStates.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Center.Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum ChangeControlStates
+{
+  [JsonStringEnumMemberName("pending")] Pending,
+  [JsonStringEnumMemberName("rejected")] Rejected,
+  [JsonStringEnumMemberName("approved")] Approved
+}
