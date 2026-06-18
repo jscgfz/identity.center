34e8f6cd16765b8888b6f85ae0602a4ef9b58512
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-06-18 09:48:29 -0500

    Add: jscg | IAlfrescoClient rest integration

---
+++ Identity.Center.Application/Abstractions/Clients/IAlfrescoClient.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+
+namespace Identity.Center.Application.Abstractions.Clients;
+
+public interface IAlfrescoClient
+{
+  //[Post("​/nodes/{nodeId}/children")]
+  [Post("/nodes/{nodeId}/children")]
+  [Multipart]
+  Task<HttpResponseMessage> PostChildren(
+    string nodeId,
+    [AliasAs("name")] string name,
+    [AliasAs("filedata")] StreamPart filedata,
+    [AliasAs("relativePath")] string? relativePath = null,
+    [AliasAs("nodeType")] string content = "cm:content",
+    [AliasAs("overwrite")] string overwrite = "false",
+    [AliasAs("cm:title")] string? title = null,
+    [AliasAs("cm:description")] string? description = null
+  );
+}
