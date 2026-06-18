78956da87e0664f574c631146ee6a07804150b4f
jscgfz <jhon.cubillos@finanzauto.com.co>
2026-06-18 10:26:04 -0500

    Add: jscg | RouteDto

---
+++ Identity.Center.Application/Features/Authentication/Dtos/RouteDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Center.Application.Features.Authentication.Dtos;
+
+public sealed record RouteDto(
+  Guid Id,
+  string Key,
+  string Name,
+  string Path,
+  bool ExcludeNav,
+  int Index,
+  string? Icon,
+  Guid? ParentId,
+  IEnumerable<string> Claims,
+  IEnumerable<RouteDto>? ChildRoutes = null
+);
